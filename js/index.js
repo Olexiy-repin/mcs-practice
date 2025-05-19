3d9201e22d287e5a6b36c49c43f32db6ab6068c6
Olexiy-repin <repinlyoha@gmail.com>
2025-05-19 14:34:12 +0300

    preparation

---
+++ js/index.js
@@ -0,0 +1,97 @@
+/*
+TODO: Перевірка віку
+TODO:
+TODO: Створи функцію checkAge(age).
+TODO: Якщо age від 18 до 60 включно — виведи "Доступ дозволено", інакше — "Доступ заборонено".
+*/
+
+// console.log(checkAge(24));
+// console.log(checkAge(17));
+// console.log(checkAge(61));
+
+/*
+TODO: Система знижок
+TODO:
+TODO: Якщо в користувача є карта постійного клієнта або він купує на суму більше 1000 — надається знижка.
+TODO: Створи функцію isDiscountAvailable(hasCard, amount).
+TODO: Параметр hasCard - бульове значення, яке сигналізує про те чи є у користувача карта постійного клієнта.
+TODO: Параметр amount - число, сума замовлення клієнта.
+TODO: Якщо hasCard = true або amount більше 1000, то функція повертає true інакше false.
+*/
+
+// console.log(isDiscountAvailable(true, 500));
+// console.log(isDiscountAvailable(false, 500));
+// console.log(isDiscountAvailable(false, 1100));
+
+/*
+TODO: Визначення пори року
+TODO:
+TODO: Створи функцію getSeason(month), яка приймає номер місяця (1–12) і повертає назву пори року.
+TODO: Наприклад: 1, 2, 12 => "Зима"
+*/
+
+// console.log(getSeason(1));
+// console.log(getSeason(5));
+// console.log(getSeason(8));
+// console.log(getSeason(10));
+
+/*
+TODO: Калькулятор
+TODO:
+TODO: Напиши функцію calculate(a, b, operator), яка виконує операцію:
+TODO: +, -, *, / — залежно від оператора. Реалізуй через switch.
+*/
+
+// console.log(calculate(10, 20, '+'));
+// console.log(calculate(10, 20, '-'));
+// console.log(calculate(10, 20, '*'));
+// console.log(calculate(10, 20, '/'));
+
+/*
+TODO: Перевірка домену пошти
+TODO:
+TODO: Напиши функцію isGmail(email), яка перевіряє, чи є email поштою на Gmail (тобто містить "@gmail.com").
+TODO: Повертає true або false.
+*/
+
+/*
+TODO: Заміна частини рядка
+TODO:
+TODO: Напиши функцію censorWord(sentence, word), яка замінює слово у реченні на "***".
+TODO: Наприклад: censorWord("You are dumb", "dumb") => "You are ***"
+*/
+
+// console.log(censorWord('You are dumb', 'dumb'));
+
+/*
+TODO: Виділення доменного імені
+TODO:
+TODO: Напиши функцію getDomain(email), яка повертає частину після символу "@" у email.
+TODO: Наприклад: getDomain("test@gmail.com") => "gmail.com"
+*/
+
+// console.log(getDomain('test@gmail.com'));
+
+/*
+TODO: Сума чисел
+TODO:
+TODO: Обчисли суму всіх чисел від 1 до n. Значення n вводиться користувачем (prompt).
+*/
+
+/*
+TODO: Пошук першого кратного 7
+TODO:
+TODO: Знайди перше число від 100 до 200, яке ділиться на 7 без остачі.
+TODO: Зупини цикл після знаходження.
+*/
+
+/*
+TODO: Паліндром
+TODO:
+TODO: Напиши функцію isPalindrome(str), яка повертає true, якщо str — паліндром.
+TODO: Наприклад: "level", "madam".
+*/
+
+// console.log(isPalindrome('level'));
+// console.log(isPalindrome('cat'));
+// console.log(isPalindrome('madam'));
