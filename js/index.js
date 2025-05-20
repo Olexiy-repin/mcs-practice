c942eca91b3b2447ecb4074e4f488c9dd219b160
Olexiy-repin <repinlyoha@gmail.com>
2025-05-20 19:22:02 +0300

    classwork

--- js/index.js
+++ js/index.js
@@ -2,26 +2,16 @@
 TODO: Перевірка віку
 TODO:
 TODO: Створи функцію checkAge(age).
-TODO: Якщо age від 18 до 60 включно — виведи "Доступ дозволено", інакше — "Доступ заборонено".
+TODO: Якщо age від 18 до 60 включно — функція повертає "Доступ дозволено", інакше — "Доступ заборонено".
 */
+
+// function checkAge(age) {
+//   return age >= 18 && age <= 60 ? 'Доступ дозволено' : 'Доступ заборонено';
+// }
 
 // console.log(checkAge(24));
 // console.log(checkAge(17));
 // console.log(checkAge(61));
-
-/*
-TODO: Система знижок
-TODO:
-TODO: Якщо в користувача є карта постійного клієнта або він купує на суму більше 1000 — надається знижка.
-TODO: Створи функцію isDiscountAvailable(hasCard, amount).
-TODO: Параметр hasCard - бульове значення, яке сигналізує про те чи є у користувача карта постійного клієнта.
-TODO: Параметр amount - число, сума замовлення клієнта.
-TODO: Якщо hasCard = true або amount більше 1000, то функція повертає true інакше false.
-*/
-
-// console.log(isDiscountAvailable(true, 500));
-// console.log(isDiscountAvailable(false, 500));
-// console.log(isDiscountAvailable(false, 1100));
 
 /*
 TODO: Визначення пори року
@@ -29,6 +19,46 @@ TODO:
 TODO: Створи функцію getSeason(month), яка приймає номер місяця (1–12) і повертає назву пори року.
 TODO: Наприклад: 1, 2, 12 => "Зима"
 */
+
+// function getSeason(month) {
+//   let seaseon = '';
+
+//   switch (month) {
+//     case 12:
+//     case 1:
+//     case 2: {
+//       seaseon = 'Зима';
+
+//       break;
+//     }
+
+//     case 3:
+//     case 4:
+//     case 5: {
+//       seaseon = 'Весна';
+
+//       break;
+//     }
+
+//     case 6:
+//     case 7:
+//     case 8: {
+//       seaseon = 'Літо';
+
+//       break;
+//     }
+
+//     case 9:
+//     case 10:
+//     case 11: {
+//       seaseon = 'Осінь';
+
+//       break;
+//     }
+//   }
+
+//   return seaseon;
+// }
 
 // console.log(getSeason(1));
 // console.log(getSeason(5));
@@ -42,6 +72,26 @@ TODO: Напиши функцію calculate(a, b, operator), яка викону
 TODO: +, -, *, / — залежно від оператора. Реалізуй через switch.
 */
 
+// function calculate(a, b, operator) {
+//   switch (operator) {
+//     case '+': {
+//       return a + b;
+//     }
+
+//     case '-': {
+//       return a - b;
+//     }
+
+//     case '*': {
+//       return a * b;
+//     }
+
+//     case '/': {
+//       return a / b;
+//     }
+//   }
+// }
+
 // console.log(calculate(10, 20, '+'));
 // console.log(calculate(10, 20, '-'));
 // console.log(calculate(10, 20, '*'));
@@ -54,12 +104,23 @@ TODO: Напиши функцію isGmail(email), яка перевіряє, ч�
 TODO: Повертає true або false.
 */
 
+// function isGmail(email) {
+//   return email.endsWith('@gmail.com');
+// }
+
+// console.log(isGmail('test@gmail.com'));
+// console.log(isGmail('test@gmail.c'));
+
 /*
 TODO: Заміна частини рядка
 TODO:
 TODO: Напиши функцію censorWord(sentence, word), яка замінює слово у реченні на "***".
 TODO: Наприклад: censorWord("You are dumb", "dumb") => "You are ***"
 */
+
+// function censorWord(sentence, word) {
+//   return sentence.toLowerCase().replaceAll(word, '***');
+// }
 
 // console.log(censorWord('You are dumb', 'dumb'));
 
@@ -70,6 +131,12 @@ TODO: Напиши функцію getDomain(email), яка повертає ча
 TODO: Наприклад: getDomain("test@gmail.com") => "gmail.com"
 */
 
+// function getDomain(email) {
+//   const indexOfChar = email.indexOf('@');
+
+//   return email.slice(indexOfChar + 1);
+// }
+
 // console.log(getDomain('test@gmail.com'));
 
 /*
@@ -78,6 +145,16 @@ TODO:
 TODO: Обчисли суму всіх чисел від 1 до n. Значення n вводиться користувачем (prompt).
 */
 
+// const max = Number(prompt('Введіть якесь число більше 1'));
+
+// let total = 0;
+
+// for (let i = 1; i <= max; i++) {
+//   total += i;
+// }
+
+// console.log(total);
+
 /*
 TODO: Пошук першого кратного 7
 TODO:
@@ -85,12 +162,34 @@ TODO: Знайди перше число від 100 до 200, яке ділит�
 TODO: Зупини цикл після знаходження.
 */
 
+// let firstEvenNumber = 0;
+
+// for (let i = 100; i <= 200; i++) {
+//   if (i % 7 === 0) {
+//     firstEvenNumber = i;
+
+//     break;
+//   }
+// }
+
+// console.log(firstEvenNumber);
+
 /*
 TODO: Паліндром
 TODO:
 TODO: Напиши функцію isPalindrome(str), яка повертає true, якщо str — паліндром.
 TODO: Наприклад: "level", "madam".
 */
+
+// function isPalindrome(str) {
+//   let reversedStr = '';
+
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     reversedStr += str[i];
+//   }
+
+//   return str === reversedStr;
+// }
 
 // console.log(isPalindrome('level'));
 // console.log(isPalindrome('cat'));
