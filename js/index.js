1ec79ef6fa4b3fe22b48ce714877b94be676fc83
Olexiy-repin <repinlyoha@gmail.com>
2025-05-20 16:54:22 +0300

    preparation

---
+++ js/index.js
@@ -0,0 +1,57 @@
+/*
+TODO: Сума елементів масиву
+TODO:
+TODO: Напиши функцію sumArray(arr), яка приймає масив чисел і повертає їхню суму.
+*/
+
+// console.log(sumArray([1, 2, 3, 4]));
+// console.log(sumArray([10, 20, 30, 40]));
+
+/*
+TODO: Пошук найбільшого елемента
+TODO:
+TODO: Створи функцію findMax(arr), яка знаходить найбільше число в масиві.
+*/
+
+// console.log(findMax([4, 7, 1, 9, 2]));
+// console.log(findMax([4, 7, -1, -9, 2]));
+
+/*
+TODO: Підрахунок кількості парних чисел
+TODO:
+TODO: Напиши функцію countEven(arr), яка повертає кількість парних чисел у масиві.
+*/
+
+// console.log(countEven([2, 5, 6, 7, 8]));
+
+/*
+TODO: Створення масиву квадратів
+TODO:
+TODO: Створи функцію squares(arr), яка повертає новий масив квадратів чисел.
+*/
+
+// console.log(squares([1, 2, 3]));
+
+/*
+TODO: Фільтрація рядків за довжиною
+TODO:
+TODO: Напиши функцію filterShortWords(words, maxLength), яка повертає масив слів, коротші за maxLength.
+*/
+
+// console.log(filterShortWords(['cat', 'elephant', 'dog'], 4));
+
+/*
+TODO: Об’єднання двох масивів без дублікатів
+TODO:
+TODO: Створи функцію mergeUnique(arr1, arr2), яка об’єднує два масиви та повертає новий масив без повторень.
+*/
+
+// console.log(mergeUnique([1, 2, 3], [3, 4, 5]));
+
+/*
+TODO: Пошук спільних елементів у двох масивах
+TODO:
+TODO: Реалізуй функцію commonElements(arr1, arr2), яка повертає масив спільних елементів.
+*/
+
+// console.log(commonElements([1, 2, 3], [2, 3, 4]));
