f0df67b128d579003ecb17cee4e63497bd60668b
Olexiy-repin <repinlyoha@gmail.com>
2025-05-23 14:06:16 +0300

    preparation

---
+++ js/index.js
@@ -0,0 +1,80 @@
+/*
+TODO: Метод для виводу інформації про товар.
+TODO:
+TODO: Створи обʼєкт product з властивостями name, price, category.
+TODO: Додай метод getInfo(), який повертає рядок типу: 'Товар: {name}, Категорія: {category}, Ціна: {price} грн'
+*/
+
+/*
+TODO: Створення методу в обʼєкті.
+TODO:
+TODO: Створи об'єкт user з властивостями firstName, lastName та age, а також методом getFullname().
+TODO: Метод має повертати повне імʼя користувача.
+*/
+
+/*
+TODO: Метод для додавання друзів.
+TODO:
+TODO: Є обʼєкт user з масивом friends.
+TODO: Додай наступні методи:
+TODO: - addFriend(friendName) - додає ім’я в список друзів.
+TODO: - removeFriend(friendName) - видаляє імʼя із списку друзів.
+*/
+
+// const user = {
+//   friends: ['Claudia', 'Justin', 'Ada', 'Lilly'],
+// };
+
+/*
+TODO: Методи для роботи з корзиною товарів.
+TODO:
+TODO: Є обʼєкт cart. У властивості items зберігається масив з товарами.
+TODO: Додай наступні методи в обʼєкт:
+TODO: - changeQuantity(productName, value) - змінює кількість товару у кошику.
+TODO: - removeProduct(productName) - видаляє товар із корзини.
+TODO: - getTotalPrice() - повартає загальну суму товарів у кошику.
+*/
+
+// const cart = {
+//   items: [
+//     { name: 'Хліб', price: 25, quantity: 3 },
+//     { name: 'Молоко', price: 30, quantity: 2 },
+//   ],
+// };
+
+/*
+TODO: Пошук за значенням.
+TODO:
+TODO: Напиши функцію findByName(users, name), яка шукає об'єкт у масиві за полем name.
+*/
+
+// console.log(
+//   findByName(
+//     [
+//       { firstName: 'Allen', age: 30, gender: 'male' },
+//       { firstName: 'Theresa', age: 20, gender: 'female' },
+//       { firstName: 'Josephine', age: 25, gender: 'female' },
+//       { firstName: 'Max', age: 40, gender: 'male' },
+//       { firstName: 'Phillip', age: 23, gender: 'male' },
+//     ],
+//     'Max'
+//   )
+// );
+
+/*
+TODO: Фільтрація об’єктів у масиві
+TODO:
+TODO: Напиши функцію filterProductsByPrice(products, threshold), яка буде приймати масив об'єктів з товарами.
+TODO: Функція має повернути лише ті, в яких ціна менше ніж параметр threshold.
+*/
+
+// console.log(
+//   filterProductsByPrice(
+//     [
+//       { name: 'Товар 1', price: 50 },
+//       { name: 'Товар 2', price: 150 },
+//       { name: 'Товар 3', price: 80 },
+//     ],
+//     100
+//   )
+// );
