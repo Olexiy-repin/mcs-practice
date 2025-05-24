e2dc08349e93a9400f8a56a4dcde1a6beb00cd7c
Olexiy-repin <repinlyoha@gmail.com>
2025-05-24 12:26:08 +0300

    preparation

---
+++ js/index.js
@@ -0,0 +1,99 @@
+/*
+TODO: Метод forEach().
+TODO:
+TODO: Виведи всі елементи масиву з індексами.
+*/
+
+// const fruits = ['apple', 'banana', 'cherry'];
+
+/*
+TODO: Метод map().
+TODO:
+TODO: Створи новий масив, у якому всі числа подвоєні.
+*/
+
+// const nums = [1, 2, 3];
+
+/*
+TODO: Метод filter().
+TODO:
+TODO: Відфільтруй користувачів віком від 18 років.
+*/
+
+// const users = [
+//   { firstName: 'Ira', age: 17 },
+//   { firstName: 'Max', age: 22 },
+//   { firstName: 'Bobby', age: 28 },
+//   { firstName: 'Anthony', age: 15 },
+// ];
+
+/*
+TODO: Метод find().
+TODO:
+TODO: Знайди користувача з email 'test@example.com'.
+*/
+
+// const users = [
+//   { firstName: 'Ann', email: 'ann@mail.com' },
+//   { firstName: 'Test', email: 'test@example.com' },
+// ];
+
+/*
+TODO: Метод some().
+TODO:
+TODO: Чи є серед товарів хоча б один з нульовою кількістю на складі?
+*/
+
+// const stock = [
+//   { title: 'Monitor', quantity: 5 },
+//   { title: 'Keyboard', quantity: 0 },
+// ];
+
+/*
+TODO: Метод every().
+TODO:
+TODO: Перевір, чи всі студенти склали тест (score > 50).
+*/
+
+// const students = [
+//   { firstName: "Ivan", score: 78 },
+//   { firstName: "Oksana", score: 91 },
+//   { firstName: "Pavlo", score: 43 }
+// ];
+
+/*
+TODO: Метод reduce().
+TODO:
+TODO: Порахуй загальну вартість товарів у кошику.
+*/
+
+// const cart = [
+//   { title: 'Book', price: 12, quantity: 2 },
+//   { title: 'Pen', price: 3, quantity: 4 },
+// ];
+
+/*
+TODO: Метод map().
+TODO:
+TODO: Для кожного працівника додай нову властивість bonus, яка дорівнює 10% від зарплати, якщо стаж > 5 років.
+*/
+
+// const employees = [
+//   { firstName: 'Fred', salary: 1000, experience: 6 },
+//   { firstName: 'Jose', salary: 800, experience: 3 },
+// ];
+
+/*
+TODO: Методи filter() та map().
+TODO: 
+TODO: Створити функцію getStudentsWithHighScore(allStudents).
+TODO: Функція буде повертати масив імен студентів, які мають середній бал > 85.
+*/
+
+// const students = [
+//   { firstName: 'Olha', grades: [90, 92, 88] },
+//   { firstName: 'Ivan', grades: [70, 75, 80] },
+//   { firstName: 'Svitlana', grades: [86, 89, 90] },
+// ];
+
+// console.log(getStudentsWithHighScore(students)); // ['Olha', 'Svitlana']
