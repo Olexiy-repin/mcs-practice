32a9fda28a856dd72c073f2c206611913babdb99
Olexiy-repin <repinlyoha@gmail.com>
2025-05-27 16:59:10 +0300

    classwork

--- js/index.js
+++ js/index.js
@@ -6,6 +6,12 @@ TODO: Виведи всі елементи масиву з індексами.
 
 // const fruits = ['apple', 'banana', 'cherry'];
 
+// console.log(fruits);
+
+// const result = fruits.forEach((el, idx, arr) => {
+//   console.log(`${idx}: ${el}`);
+// });
+
 /*
 TODO: Метод map().
 TODO:
@@ -13,6 +19,10 @@ TODO: Створи новий масив, у якому всі числа под
 */
 
 // const nums = [1, 2, 3];
+// const doubleNumbers = nums.map(el => el * 2);
+
+// console.log(nums);
+// console.log(doubleNumbers);
 
 /*
 TODO: Метод filter().
@@ -26,6 +36,10 @@ TODO: Відфільтруй користувачів віком від 18 ро�
 //   { firstName: 'Bobby', age: 28 },
 //   { firstName: 'Anthony', age: 15 },
 // ];
+// const filteredUsers = users.filter(el => el.age >= 18);
+
+// console.table(users);
+// console.table(filteredUsers);
 
 /*
 TODO: Метод find().
@@ -37,6 +51,10 @@ TODO: Знайди користувача з email 'test@example.com'.
 //   { firstName: 'Ann', email: 'ann@mail.com' },
 //   { firstName: 'Test', email: 'test@example.com' },
 // ];
+// const user = users.find(el => el.email === 'test@example.com');
+
+// console.table(users);
+// console.log(user);
 
 /*
 TODO: Метод some().
@@ -48,6 +66,10 @@ TODO: Чи є серед товарів хоча б один з нульовою
 //   { title: 'Monitor', quantity: 5 },
 //   { title: 'Keyboard', quantity: 0 },
 // ];
+// const isSomeProductOutOfStock = stock.some(el => el.quantity === 0);
+
+// console.table(stock);
+// console.log(isSomeProductOutOfStock);
 
 /*
 TODO: Метод every().
@@ -56,10 +78,14 @@ TODO: Перевір, чи всі студенти склали тест (score 
 */
 
 // const students = [
-//   { firstName: "Ivan", score: 78 },
-//   { firstName: "Oksana", score: 91 },
-//   { firstName: "Pavlo", score: 43 }
+//   { firstName: 'Ivan', score: 78 },
+//   { firstName: 'Oksana', score: 91 },
+//   { firstName: 'Pavlo', score: 45 },
 // ];
+// const isAllStudentsApproved = students.every(el => el.score > 50);
+
+// console.table(students);
+// console.log(isAllStudentsApproved);
 
 /*
 TODO: Метод reduce().
@@ -73,15 +99,101 @@ TODO: Порахуй загальну вартість товарів у кош�
 // ];
 
 /*
-TODO: Метод map().
-TODO:
-TODO: Для кожного працівника додай нову властивість bonus, яка дорівнює 10% від зарплати, якщо стаж > 5 років.
+1 Ітерація
+  acc = 0;
+  el = { title: 'Book', price: 12, quantity: 2 };
+  idx = 0;
+  arr = cart;
+
+  return 24;
+
+2 Ітерація
+  acc = 24;
+  el = { title: 'Pen', price: 3, quantity: 4 };
+  idx = 1;
+  arr = cart;
+
+  return 36;
 */
 
-// const employees = [
-//   { firstName: 'Fred', salary: 1000, experience: 6 },
-//   { firstName: 'Jose', salary: 800, experience: 3 },
-// ];
+// const totalPrice = cart.reduce((acc, el) => acc + el.price * el.quantity, 0);
+
+// console.table(cart);
+// console.log(totalPrice);
+
+/*
+TODO: Метод map().
+TODO:
+TODO: Створи функцію addBonus(employees)
+TODO: Функція має для кожного працівника додати нову властивість bonus, яка дорівнює 10% від зарплати, якщо стаж > 5 років.
+*/
+
+// const addBonus = employees => {
+//   return employees.map(el => {
+//     el.bonus = el.experience > 5 ? el.salary * 0.1 : 0;
+
+//     return el;
+//   });
+// };
+
+// console.log(
+//   addBonus([
+//     { firstName: 'Fred', salary: 1000, experience: 6 },
+//     { firstName: 'Jose', salary: 800, experience: 3 },
+//   ])
+// );
+
+/*
+TODO: Сортування товарів за кількістю коментарів
+TODO:
+TODO: Створи функцію sortByDescendingCommentsCount(products).
+TODO: Функція має повертати масив з товарами відсортованих за кількістю коментарів. 
+*/
+
+// const sortByDescendingCommentsCount = products => {
+//   console.table(products);
+
+//   return products.toSorted((productA, productB) => {
+//     return productB.comments.length - productA.comments.length;
+//   });
+// };
+
+// console.table(
+//   sortByDescendingCommentsCount([
+//     {
+//       name: 'Ноутбук',
+//       price: 27000,
+//       comments: [
+//         { description: 'comment 1', rating: 4 },
+//         { description: 'comment 2', rating: 3.5 },
+//         { description: 'comment 3', rating: 4 },
+//       ],
+//     },
+//     {
+//       name: 'Смартфон',
+//       price: 19000,
+//       comments: [
+//         { description: 'comment 1', rating: 4 },
+//         { description: 'comment 2', rating: 5 },
+//       ],
+//     },
+//     {
+//       name: 'Планшет',
+//       price: 14000,
+//       comments: [
+//         { description: 'comment 1', rating: 4 },
+//         { description: 'comment 2', rating: 3.5 },
+//         { description: 'comment 3', rating: 4 },
+//         { description: 'comment 4', rating: 4.5 },
+//       ],
+//     },
+//     {
+//       name: 'Монітор',
+//       price: 8500,
+//       comments: [{ description: 'comment 1', rating: 3 }],
+//     },
+//   ])
+// );
 
 /*
 TODO: Методи filter() та map().
@@ -90,10 +202,15 @@ TODO: Створити функцію getStudentsWithHighScore(allStudents).
 TODO: Функція буде повертати масив імен студентів, які мають середній бал > 85.
 */
 
+// const getStudentsWithHighScore = allStudents => {
+//   return allStudents.filter(student => student.averageScore > 85).map(student => student.firstName);
+// };
+
 // const students = [
-//   { firstName: 'Olha', grades: [90, 92, 88] },
-//   { firstName: 'Ivan', grades: [70, 75, 80] },
-//   { firstName: 'Svitlana', grades: [86, 89, 90] },
+//   { firstName: 'Olha', averageScore: 90 },
+//   { firstName: 'Ivan', averageScore: 75 },
+//   { firstName: 'Svitlana', averageScore: 88 },
 // ];
 
+// console.table(students);
 // console.log(getStudentsWithHighScore(students)); // ['Olha', 'Svitlana']
