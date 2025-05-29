074b0c95ac2c7d4ff16be9151b57d49c14609a74
Olexiy-repin <repinlyoha@gmail.com>
2025-05-29 17:11:34 +0300

    preparation

---
+++ js/02-friends.js
@@ -0,0 +1,42 @@
+/*
+TODO: Є масив friends, по якому потрібно згенерувати картки з друзями і вставити в список на сторінці.
+TODO: Використовуйте document.createElement() та el.append().
+TODO: Розмітка однієї карточки має бути наступною:
+  <li class="friends-list-item">
+    <img class="friend-avatar" src="" alt="">
+    <h2 class="friend-name"></h2>
+  </li>
+*/
+
+const friends = [
+  {
+    fullName: 'Lester Warren',
+    avatarUrl: 'https://placehold.co/32x32/orange/white?text=LW',
+    altDescription: 'Lester Warren',
+  },
+  {
+    fullName: 'Nannie Garrett',
+    avatarUrl: 'https://placehold.co/32x32/orange/white?text=NG',
+    altDescription: 'Nannie Garrett',
+  },
+  {
+    fullName: 'Bill Moss',
+    avatarUrl: 'https://placehold.co/32x32/orange/white?text=BM',
+    altDescription: 'Bill Moss',
+  },
+  {
+    fullName: 'Craig Curry',
+    avatarUrl: 'https://placehold.co/32x32/orange/white?text=CC',
+    altDescription: 'Craig Curry',
+  },
+  {
+    fullName: 'Antonio Russell',
+    avatarUrl: 'https://placehold.co/32x32/orange/white?text=AR',
+    altDescription: 'Antonio Russell',
+  },
+  {
+    fullName: 'Lela Jennings',
+    avatarUrl: 'https://placehold.co/32x32/orange/white?text=LJ',
+    altDescription: 'Lela Jennings',
+  },
+];
